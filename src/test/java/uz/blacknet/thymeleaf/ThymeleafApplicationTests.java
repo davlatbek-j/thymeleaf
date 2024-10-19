6d3c777639111c11df9908113867b8edc20c4b5d
SuperHuma <theferganes@gmail.com>
2024-10-19 18:51:51 +0500

    bismillah

---
+++ src/test/java/uz/blacknet/thymeleaf/ThymeleafApplicationTests.java
@@ -0,0 +1,15 @@
+package uz.blacknet.thymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
