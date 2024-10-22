ea192aaa25028be4a54988385bc9b6e1a1c46e76
SuperHuma <theferganes@gmail.com>
2024-10-22 12:29:55 +0500

    path creater

---
+++ src/main/java/uz/blacknet/thymeleaf/config/DataLoader.java
@@ -0,0 +1,33 @@
+package uz.blacknet.thymeleaf.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.io.File;
+
+@Component
+public class DataLoader implements CommandLineRunner
+{
+    private static final Logger log = LoggerFactory.getLogger(DataLoader.class);
+    @Value("${photo.upload.path}")
+    private String uploadPath;
+
+    @Override
+    public void run(String... args) throws Exception
+    {
+        createPhotoPath();
+    }
+
+    private void createPhotoPath()
+    {
+        File directory = new File(uploadPath);
+        if (!directory.exists())
+        {
+            if (directory.mkdirs())
+                log.info("Directory Created with path : {}", directory.getAbsolutePath());
+        }
+    }
+}
